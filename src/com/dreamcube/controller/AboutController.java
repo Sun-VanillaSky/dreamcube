822cac00ca80041dbc196a3cd233bf1aacd2d168
tqhuang <466355917@qq.com>
2018-06-27 23:39:36 +0800

    tqhuang

--- src/com/dreamcube/controller/AboutController.java
+++ src/com/dreamcube/controller/AboutController.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 import com.dreamcube.commons.util.http.HttpUtils;
 
 
-
 @WebServlet("/about" )  
 public class AboutController  extends HttpServlet {
 
